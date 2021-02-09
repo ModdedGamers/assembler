1753117bb06b565e2478159f53643b96fa89a170
Mr Hedgehog <mrhedgehog0@protonmail.com>
2021-02-09 09:33:32 -0500

    Update to deprecate yaml config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,43 @@
-use clap::{App, load_yaml};
+//This code prioritizes readability over length.
+//I intend to make this easy to maintain and add features to in the future.
+
+use clap::{App, AppSettings, Arg, crate_authors, crate_version};
 
 fn main() {
-	let yaml = load_yaml!("clap.yaml");
-	let matches = App::from(yaml).get_matches();
+	let matches = App::new("mmf-rs")
+		.version(crate_version!())
+		.author(crate_authors!())
+		.about("A mod manager for Factorio, written in Rust!")
+		.setting(AppSettings::ArgRequiredElseHelp)
+
+
+		.arg(
+			Arg::new("verbose")
+				.about("Print every action the program takes as it is performed.")
+				.short('v')
+				.long("verbose")
+		)
+		.subcommand(
+			App::new("add")
+				.about("Add a mod to the current modpack")
+				.aliases(&["i", "install", "add"])
+		)
+
+		.subcommand(
+			App::new("remove")
+				.about("Remove a mod from the current modpack")
+				.aliases(&["u", "uninstall", "remove"])
+		)
+
+		.subcommand(
+			App::new("start")
+			.about("Start factorio with the current modpack")
+			.aliases(&["s", "start", "launch"])
+			
+		)
+
+
+		.get_matches();
+
 
 }
